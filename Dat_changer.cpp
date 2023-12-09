e8b931066658ff542289b0090b22f78d55bbf499
MRuzhevich <mazaahakaa@gmail.com>
2023-12-09 18:59:31 +0300

    Добавьте файлы проекта.

---
+++ Dat_changer.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	char buff[50];
+	ifstream rfile("test1.dat");
+
+	/*rfile >> buff;
+	cout << buff << endl;*/
+
+	rfile.getline(buff, 50);
+	//rfile.close();
+	cout << buff << endl;
+
+
+	system("pause>0");
+}
