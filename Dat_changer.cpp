29f46d85904d0e961ec66397f1c6ec1d72d9e492
MRuzhevich <mazaahakaa@gmail.com>
2023-12-16 09:02:32 +0300

    feat: added custom function for split string by spaces

--- Dat_changer.cpp
+++ Dat_changer.cpp
@@ -1,21 +1,59 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+vector<string> splitString(string spacestr) {
+	string nstr = "";
+	vector<string> words;
+	for (auto x : spacestr) {
+		if (x == ' ') {
+			if (!nstr.empty()) {
+				words.push_back(nstr);
+				nstr = "";
+			}
+		}
+		else {
+			nstr += x;
+		}
+	}
+
+	if (!nstr.empty()) {
+		words.push_back(nstr);
+	}
+
+	return words;
+}
+
+
 int main()
 {
-    char buff[50];
-    ifstream rfile("test1.dat");
-    ofstream wfile("test_new.dat");
+	char buff[50];
+	ifstream rfile("test1.dat");
+	ofstream wfile("test_new.dat");
 
-    while (rfile.getline(buff, 50)) {
-        cout << buff << endl;
-        wfile << buff << endl;
+	while (rfile.getline(buff, 50)) {
+		string first = "";
+		string second = "";
+		wfile << buff << endl;
+		string str(buff);
+		//cout << str << endl;
 
-    }
+		vector <string> vec = splitString(str);
 
-    wfile.close();
+		first = vec[0];
+		second = vec[1];
 
-    system("pause>0");
+		int nfirst = stoi(first);
+		int nsecond = stoi(second);
+
+		cout << nfirst << endl;
+		cout << nsecond << endl;
+	}
+
+	wfile.close();
+
+	system("pause>0");
 }
